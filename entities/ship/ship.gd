12140db8c45b75e2c23a1b4ea509664d366048a7
fio <the_oronco@posteo.net>
2026-04-15 13:58:03 +0200

    added velocity limits and strafing

--- entities/ship/ship.gd
+++ entities/ship/ship.gd
@@ -1,8 +1,14 @@
 extends RigidBody2D
 
+@export_group("movement")
+@export_range(1, 3000) var thruster_power: float = 500.
+@export_range(1, 3000) var strafe_power: float = 500.
+@export_range(.1, 5) var rotation_speed: float = 500.
+@export_range(1, 5000) var max_velocity: float = 1000.
+@export_range(1, 5000) var max_force: float = 1000.
+@export_group("shooting")
 @export var shot: PackedScene
-@export var thruster_power: float = 20.
-@export var rotation_speed: float = 80.
+
 @onready var thruster_particles: GPUParticles2D = $ThrusterParticles
 @onready var muzzle: Marker2D = %Muzzle
 
@@ -10,23 +16,37 @@ var _screen_size: Vector2:
 	get: return self.get_viewport_rect().size
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_up"):
+	_rotate(delta)
+	_thrust()
+	_strafe()
+	_fire()
+		
+	self.position = MoveUtils.screen_wrap(self.position, _screen_size)
+	
+	print(self.linear_velocity.normalized())
+	print(linear_velocity.length())
+	self.linear_velocity = self.linear_velocity.normalized() * clamp(linear_velocity.length(), 0 , max_velocity) 
+
+func _rotate(delta: float) -> void:
+	var rotation_input: float = Input.get_axis("rotate_left", "rotate_right")
+	if abs(rotation_input) >= 0.1 :
+		self.rotate(rotation_input * rotation_speed * delta)
+
+func _thrust() -> void:
+	if Input.is_action_pressed("thrust"):
 		var thrust = (Vector2.UP * thruster_power).rotated(self.rotation)
 		self.apply_central_force(thrust)
 		thruster_particles.emitting = true
 	else: 
 		thruster_particles.emitting = false
 
-	var rotation_input: float = Input.get_axis("ui_left", "ui_right")
-	if abs(rotation_input) >= 0.1 :
-		self.apply_torque(rotation_input * rotation_speed)
-		
-	if Input.is_action_just_pressed("ui_accept"):
+func _strafe() -> void:
+	var strafe := Input.get_axis("strafe_left", "strafe_right")
+	self.apply_central_force(self.transform.x * strafe * strafe_power)
+
+func _fire() -> void:
+	if Input.is_action_just_pressed("fire"):
 		var bullet: Shot = shot.instantiate()
 		bullet.transform = muzzle.global_transform
 		bullet.linear_velocity = self.linear_velocity
 		get_parent().add_child(bullet)
-		
-	self.position = MoveUtils.screen_wrap(self.position, _screen_size)
-
-		
