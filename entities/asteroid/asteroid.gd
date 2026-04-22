056a753c7631bf397f13b1517f9dcf48d3b34d95
obloneon <232438585+obloneon@users.noreply.github.com>
2026-04-22 13:41:22 +0200

    completed asteroid splitting

--- entities/asteroid/asteroid.gd
+++ entities/asteroid/asteroid.gd
@@ -10,10 +10,12 @@ var _screen_size: Vector2:
 var splits: bool = true
 
 ## Amount of asteroids generated on split
-@export var split_count: int = 2:
+@export var split_count: int = 5:
 	set(value): 
 		value = clamp(value, 1, 6)
 		split_count = value
+@export var split_force: int = 500
+@export var min_radius: float = 10
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var icon: Sprite2D = $Icon
@@ -36,33 +38,35 @@ func split(body: Node) -> void:
 		return
 	var radius: float = collision_shape_2d.shape.radius
 	var rotation_delta: float = 2. * PI / split_count
-	var rng = RandomNumberGenerator.new()
-	var random_rotatation := rng.randf()
+	var random_rotatation :=  RandomNumberGenerator.new().randf()
+	var unit_circle_radius := _calc_circle_radius(split_count)
+	var new_radius := radius * unit_circle_radius
+	if new_radius < min_radius:
+		self.queue_free()
+		return
+	var original_direction: Vector2 = self.get_linear_velocity().normalized()
+	if original_direction == Vector2.ZERO:
+		original_direction = Vector2.UP
 	for i in range(split_count):
 		var asteroid_instance := ASTEROID.instantiate() as Asteroid
-		var unit_circle_radius := _calc_circle_radius(split_count)
-		var new_radius := radius * unit_circle_radius
-		var original_direction: Vector2 = self.get_linear_velocity().normalized()
-		if original_direction == Vector2.ZERO:
-			original_direction = Vector2.UP
+		var new_collision_shape := CircleShape2D.new()
 		var push_direction := original_direction.rotated(
 			rotation_delta * i * random_rotatation
 		)
 		get_parent().add_child.call_deferred(asteroid_instance)
 		await asteroid_instance.ready
 		asteroid_instance.icon.apply_scale(self.icon.scale * unit_circle_radius)
-		var new_collision_shape = CircleShape2D.new()
 		new_collision_shape.radius = new_radius
 		asteroid_instance.collision_shape_2d.shape = new_collision_shape
 		asteroid_instance.position = position + push_direction * (radius - new_radius)
-		asteroid_instance.apply_central_impulse(push_direction)
-		self.queue_free()
+		asteroid_instance.apply_central_impulse(push_direction * split_force)
+	self.queue_free()
 
 func _calc_circle_radius(circle_count: int) -> float:
 	match circle_count:
 		2: return 1. / 2.
 		3: return 1. / (1. + (2. / sqrt(3.)))
 		4: return 1. / (1. + sqrt(2.))
-		5: return 1. / 1. + sqrt(2. * (1. + (1. / sqrt(5.))))
+		5: return 1. / (1. + sqrt(2. * (1. + (1. / sqrt(5.)))))
 		6: return 3.
 		_: return 1.
