2eb949d0b17423008c93609d70d771994bc50e25
fio <the_oronco@posteo.net>
2026-04-22 11:40:02 +0200

    the boundaries now grow with the viewport

--- entities/wrap/wrap.gd
+++ entities/wrap/wrap.gd
@@ -9,7 +9,12 @@ extends Area2D
 var _screen_size: Vector2
 
 func _ready() -> void:
+	get_viewport().size_changed.connect(move_boundaries_to_screen_border)
+	move_boundaries_to_screen_border()
+
+func move_boundaries_to_screen_border() -> void:
 	_screen_size = self.get_viewport_rect().size
+
 	top.position.x = _screen_size.x / 2
 	
 	left.position.y = _screen_size.y / 2
