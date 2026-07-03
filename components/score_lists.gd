362ceef866d73ac1cb5dc43032a739a39dfa690b
obloneon <232438585+obloneon@users.noreply.github.com>
2026-07-03 14:48:22 +0200

    added highscore sorting, some fixes and a test function.

--- components/score_lists.gd
+++ components/score_lists.gd
@@ -9,12 +9,14 @@ const FILE_PATH: String = "user://highscore_lists.tres"
 
 
 static func load_or_create() -> ScoreLists:
-	var res := ResourceLoader.load(FILE_PATH) as ScoreLists
+	var res = ResourceLoader.load(FILE_PATH) if FileAccess.file_exists(FILE_PATH) else null
 	if res != null and res is ScoreLists:
 		return res
 	return ScoreLists.new()
 
 
 func save_score(game_mode: String, entry_name: String, score: int) -> void:
-	list_dict.game_mode.entry_name = score
+	if not list_dict.has(game_mode):
+			list_dict[game_mode] = {}
+	list_dict[game_mode][entry_name] = score
 	ResourceSaver.save(self, FILE_PATH)
